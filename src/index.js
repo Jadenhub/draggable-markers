afcedb872fd8e09f23d2f773ba322dc61babde24
Roy <cjh19891018@gmail.com>
2020-01-02 14:33:48 +0800

    add index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './components/layout';
+import './index.css';
+
+ReactDOM.render(<Layout />, document.getElementById('root'));
