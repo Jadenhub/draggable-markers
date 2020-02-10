b6d601a43394b08912eace220bdf05253da79f9b
Roy <cjh19891018@gmail.com>
2020-02-10 16:16:55 +0800

    1. lazy bind onMouseMove and handleDragEnd method 2. Make the handleDrag method only focus on marker drag (Single responsibility principle)

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,6 @@ const throttle = function(fn, interval){
   }
 };
 
-
 export default class Layout extends React.Component {
   constructor(props){
     super(props);
@@ -72,36 +71,34 @@ export default class Layout extends React.Component {
   }
 
   handleDrag = (e) =>{
-    if (this.state.isDragging) {
-      const markers = this.state.markers.slice();
-      const clientpos = this.state.clientpos;
-      const targetKey = this.state.target;
-      const idx = markers.findIndex((item)=>{
-        return item.key=== targetKey;
+    const markers = this.state.markers.slice();
+    const clientpos = this.state.clientpos;
+    const targetKey = this.state.target;
+    const idx = markers.findIndex((item)=>{
+      return item.key=== targetKey;
+    });
+    const coordinate = markers[idx].coordinate.slice();
+    coordinate[0] += e.clientY - clientpos.y;
+    coordinate[1] += e.clientX - clientpos.x;
+    if (coordinate[0] <= this.state.range.maxY && coordinate[0] >= 0
+      && coordinate[1] <= this.state.range.maxX && coordinate[1] >= 0) {
+      markers[idx].coordinate = coordinate;
+      this.setState({
+        markers: markers,
+        clientpos: {
+          y: e.clientY,
+          x: e.clientX
+        }
       });
-      const coordinate = markers[idx].coordinate.slice();
-      coordinate[0] += e.clientY - clientpos.y;
-      coordinate[1] += e.clientX - clientpos.x;
-      if (coordinate[0] <= this.state.range.maxY && coordinate[0] >= 0
-        && coordinate[1] <= this.state.range.maxX && coordinate[1] >= 0) {
-        markers[idx].coordinate = coordinate;
-        this.setState({
-          markers: markers,
-          clientpos: {
-            y: e.clientY,
-            x: e.clientX
-          }
-        });
-      } else {
-        this.setState({
-          clientpos: {
-            y: e.clientY,
-            x: e.clientX,
-          },
-          isDragging: false,
-        });
-      }
-    };
+    } else {
+      this.setState({
+        clientpos: {
+          y: e.clientY,
+          x: e.clientX,
+        },
+        isDragging: false,
+      });
+    }
   }
 
   _throttledMouseMove = throttle(this.handleDrag, 10);
@@ -110,13 +107,11 @@ export default class Layout extends React.Component {
     e.persist();
     this._throttledMouseMove(e);
   }
-
   handleDragEnd = (e) => {
     this.setState({
       isDragging: false,
     });
   }
-
   handleImgLoad = ({target:img}) =>{
     this.setState({
       range:{
@@ -130,6 +125,7 @@ export default class Layout extends React.Component {
 
   render(){
     const markers = this.state.markers;
+    const isDragging = this.state.isDragging;
     const pins =  markers.map((marker, idx)=>{
       return (
        <div key={marker.key} className="map-marker"
@@ -139,18 +135,18 @@ export default class Layout extends React.Component {
             cursor: (this.state.isDragging)?'move': 'pointer',
           }}
           onMouseDown={(e)=>{this.handleDragStart(e);}}
-          onMouseUp={(e)=>{this.handleDragEnd(e);}}
+          onMouseUp={(isDragging)? this.handleDragEnd: null}
         >
           <img alt='pin' value={marker.key} src={icon}/>
        </div>
       );
     })
-    //          {/* onMouseMove={(e)=>{this.handleDrag(e);}}  */}
+
     return (
       <>
         <h3 className='title'>React Draggable Component</h3>
         <div className='layout'
-          onMouseMove={this.onMouseMove} 
+          onMouseMove={(isDragging)? this.onMouseMove: null} 
         >
           <img src={map} alt='map' onClick={(e)=>{this.handleImgClick(e);}} onLoad={(e)=>{this.handleImgLoad(e)}}/>
           <div>
